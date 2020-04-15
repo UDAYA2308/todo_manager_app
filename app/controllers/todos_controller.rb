b2c6fcddb9a74c3f8026df2de51ec325668cb623
udaya2308 <udaya14apr2000@gmail.com>
2020-04-15 10:12:38 +0530

    added form to savve new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -19,8 +19,7 @@ class TodosController < ApplicationController
       due_date: due_date,
       completed: false,
     )
-    response_text = "Hey , your new todo is created with id #{todo_new.id}"
-    render plain: response_text
+    redirect_to todos_path
   end
 
   def update

be6767224679afc6f14b229ef5efe31f4ad7c734
udaya2308 <udaya14apr2000@gmail.com>
2020-04-30 11:49:16 +0530

    added sign-out for the user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "users/login", to: "users#login"
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin", to: "sessions#create", as: :sessions
+  delete "/signout", to: "sessions#destroy", as: :destroy_session
   get "/", to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todos

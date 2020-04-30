c18160028ac5560476766ffced38adb6a17e274d
udaya2308 <udaya14apr2000@gmail.com>
2020-04-30 11:21:40 +0530

    added bcrypt to store password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
+      password: params[:password],
     )
 
     redirect_to "/"

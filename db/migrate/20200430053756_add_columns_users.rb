9ba26b367f080e67bb91131c09dd2334e2be8f68
udaya2308 <udaya14apr2000@gmail.com>
2020-04-30 11:55:40 +0530

    updated db migrations

--- db/migrate/20200430053756_add_columns_users.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :name, :string
-    remove_column :users, :password, :string
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :password_digest, :string
-  end
-end

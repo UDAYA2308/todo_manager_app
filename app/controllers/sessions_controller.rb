ea6ef0e4633dfced765948c1479b2d0d3e1b110f
udaya2308 <udaya14apr2000@gmail.com>
2020-04-30 11:25:24 +0530

    added simple sigin page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user and user.authenticate(params[:password])
+      redirect_to "/"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   post "users/login", to: "users#login"
   get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin", to: "sessions#create", as: :sessions
   get "/", to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todos

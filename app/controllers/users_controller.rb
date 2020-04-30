99614c504e87ea2a150c942259be80b869876d86
udaya2308 <udaya14apr2000@gmail.com>
2020-04-30 12:39:10 +0530

    validate user details before saving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,14 +34,19 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    user_new = User.create!(
+    user_new = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    session[:current_user_id] = user_new.id
-    redirect_to "/"
+    if user_new.save
+      session[:current_user_id] = user_new.id
+      redirect_to "/"
+    else
+      flash[:error] = user_new.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 
   # PATCH/PUT /users/1
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :first_name, length: { minimum: 4 }
+  validates :email, presence: true
   has_secure_password
   has_many :todos
 
